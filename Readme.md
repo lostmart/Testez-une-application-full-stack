5231b7ddf2dc74757600731ff91a80e191250db5
lostmart <martinpedraza1979@gmail.com>
2025-06-28 15:45:15 +0200

    readme edits and global variables

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Savanza - Yoga Fullstack App
+
+## Technos and versions
+
+- Java 11
+- NodeJS 16
+- MySQL
+- Angular CLI 14
--- back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
+++ back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootSecurityJwtApplication {
 	public static void main(String[] args) {
-    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.filename(".env")
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
 }
